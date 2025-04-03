b5739cdcc4e66e204973ad9fc501558574a5e561
nina02-hub <nina02@my.yorku.ca>
2025-04-03 17:48:54 -0400

    timer finished

--- static/js/timer.js
+++ static/js/timer.js
@@ -1,6 +1,15 @@
+var intervalId = null;
+var workTimer = 60*25;
+var breakTimer = 60*5;
+var count = 0;
+var currentTimer = workTimer;
+var pauseTime = 0;
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
-    var intervalId = setInterval(function () {
+    if (intervalId) {
+        return;
+    }
+    intervalId = setInterval(function () {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
 
@@ -10,21 +19,63 @@ function startTimer(duration, display) {
         display.textContent = minutes + ":" + seconds;
 
         if (--timer < 0) {
-            timer = duration;
+            clearInterval(intervalId);
+            intervalId = null;
+
+            if (count < 7) {
+                count++;
+                if (count%2 === 1) {
+                    alert("Break Time!");
+                    currentTimer = breakTimer;
+                } 
+                else {
+                    alert("Back to Work!");
+                    currentTimer = workTimer;
+                }
+                startTimer(currentTimer, display);
+            }
         }
-    }, 1000);
-    
-      document.getElementById('stop').addEventListener('click', function () {
-          clearInterval(intervalId);
-      });
+
+        
+    }, 1);
 }
 
 window.onload = function () {
-    var count = 60 * 25,
-        display = document.querySelector('#time');
+    var display = document.querySelector('#time');
        // display.textContent = "00:"+count;
-      document.getElementById('start').addEventListener('click', function () { 
-    		startTimer(count, display);
-      });       
+        display.textContent = "25:00";
+        document.getElementById('start').addEventListener('click', function () { 
+        if (intervalId == null) {
+            if (pauseTime > 0) {
+                startTimer(pauseTime, display);
+                pauseTime = 0;
+            }
+            else {
+                startTimer(currentTimer, display);
+            }
+            
+        }
+    	
+      });
 
-    };
+      document.getElementById('stop').addEventListener('click', function () {
+        if (intervalId !== null) {
+            clearInterval(intervalId);
+            minutes = parseInt(display.textContent.split(":")[0], 10);
+            seconds = parseInt(display.textContent.split(":")[1], 10);
+            pauseTime = minutes * 60 + seconds;
+            intervalId = null;
+        }
+
+      });
+      
+      document.getElementById('reset').addEventListener('click', function () {
+        clearInterval(intervalId);
+        intervalId = null;
+        currentTimer = workTimer;
+        count = 0;
+        pauseTime = 0;
+        display.textContent = "25:00";
+      });
+
+};
